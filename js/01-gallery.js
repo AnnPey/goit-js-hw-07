48ee64729aa69179a89fcd78bae92c184a2ecfbe
AnnPey <peycheva440@gmail.ua>
2023-11-18 16:03:21 +0200

    7 end

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,18 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const container = document.querySelector('.gallery');
+//console.log(galleryItems)
+const markGallery = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="galery__item"> 
+        <a class = "image__link" href = "${original}" >
+    <img class = "gallery__image" src= "${preview}" 
+    data-source ="${original}"
+    alt = "${description}" 
+     />
+    </a>
+    </li>`
+  )
+  .join('');
+container.insertAdjacentHTML('beforeend', markGallery);
+container.addEventListener('click', onClick);
+function onClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+  const modalImg = event.target.dataset.source;
 
-console.log(galleryItems);
-
-const gallery = document.querySelector(".gallery");
-
-const createGalleryItem = ({preview, original, description}) => {
-    return `<li class="gallery__item">
-                <a class="gallery__link" href="${original}">
-                    <img class="gallery__image" src="${preview}" loading="lazy" alt="${description}" width="300px"/>
-                </a>
-            </li>`;
-    };
-
-const resoult = galleryItems.map(createGalleryItem).join("");
-gallery.innerHTML = resoult;
+  const modalOn = basicLightbox.create(`<img src="${modalImg}" width="800" height="600"/>`, {
+    onShow: () => {
+      document.addEventListener('keydown', OnEscClose);
+    },
+    onClose: () => {
+      document.removeEventListener('keydown', OnEscClose);
+    },
+  });
+  modalOn.show();
+  function OnEscClose(event) {
+    if (event.code === 'Escape') return modalOn.close();
+  }
+}
 
